0a6013df7854ad7659f7435295eca66f9c35e0aa
mehmetCt <mehmet@cybertekschool.com>
2024-08-19 19:00:32 -0400

    Project Init-FT8

--- src/test/java/com/cydeo/tests/P01_SimpleGetRequest.java
+++ src/test/java/com/cydeo/tests/P01_SimpleGetRequest.java
@@ -1,4 +1,33 @@
 package com.cydeo.tests;
 
 public class P01_SimpleGetRequest {
+
+
+    /*
+
+    DAY 2 -> RESPONSE.PATH VS JSONPATH METHODS
+             PATH PARAM VS QUERY PARAM
+
+    DAY 3 -> POJO
+             DESERIALIZATION -> GET
+             SERIALIZATION -> POST -> DELETE
+
+    DAY 4 -> AUTHORIZATION
+             PREPARATION FOR CUCUMBER
+
+    DAY 5 -> SHARE READY REPO.
+             - LOGS -> LOG4J2
+             - E2E -> UI - DB - API
+             - API
+
+   POSTMAN AUTOMATION -> JAMAL RECORDINGS
+
+
+
+     */
+
+
+
+
+
 }
