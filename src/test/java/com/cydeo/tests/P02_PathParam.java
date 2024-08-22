3be19927310b26aca6aec308b76c3c369efc4b56
mehmetCt <mehmet@cybertekschool.com>
2024-08-22 17:33:20 -0400

    Day02

--- src/test/java/com/cydeo/tests/P02_PathParam.java
+++ src/test/java/com/cydeo/tests/P02_PathParam.java
@@ -3,6 +3,7 @@ package com.cydeo.tests;
 import com.cydeo.util.FakeStoreTestBase;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -85,6 +86,38 @@ public class P02_PathParam extends FakeStoreTestBase {
 
         //     * - Category name is "Electronics"
         Assertions.assertEquals("Electronics",response.path("category.name"));
+
+    }
+
+
+
+    @Test
+    public void task2() {
+
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .pathParam("id", 144)
+                .when().get("/api/v1/products/{id}");
+
+        response.prettyPrint();
+
+
+        // Init JSONPATH OBJECT
+        JsonPath jp = response.jsonPath();
+
+        //     * - Status code should be 200
+        Assertions.assertEquals(200,response.statusCode());
+
+        //     * - Content Type is application/json; charset=utf-8
+        Assertions.assertEquals(ContentType.JSON.withCharset("utf-8"),response.contentType());
+
+        //     * - id is 144
+        Assertions.assertEquals(144,jp.getInt("id"));
+
+        //     * - Title is "Laptop"
+        Assertions.assertEquals("Laptop",jp.getString("title"));
+
+        //     * - Category name is "Electronics"
+        Assertions.assertEquals("Electronics",jp.getString("category.name"));
 
     }
 }
