12fda4d3004a2a962ea0beaed218f5f27b1040e1
mehmetCt <mehmet@cybertekschool.com>
2024-08-22 18:44:38 -0400

    Day02

--- src/test/java/com/cydeo/tests/P03_QueryParam.java
+++ src/test/java/com/cydeo/tests/P03_QueryParam.java
@@ -1,9 +1,32 @@
 package com.cydeo.tests;
 
 import com.cydeo.util.FakeStoreTestBase;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class P03_QueryParam extends FakeStoreTestBase {
+
+    /*
+    QUERY PARAM
+      - It is not part of URL
+      - It is for filtering data based on provided
+         Key and Value
+      - It starts after ?
+
+      /api/v1/products
+      ? -> end of URL
+      limit=10 -> QueryParam
+      &
+      offset=0 -> QueryParam
+     */
+
+
+
     /*
      * 1- Given accept type is Json
      * 2- Query Parameters value is
@@ -16,12 +39,58 @@ public class P03_QueryParam extends FakeStoreTestBase {
      * - Each product has id
      * - Each product has category id
      * - Get all product names
-     * - Get product ids
+     * - Get all product ids
      * - Get all category names
      */
 
     @Test
     public void task1() {
+
+        JsonPath jp = RestAssured.given().accept(ContentType.JSON)
+                .queryParam("limit", 10)
+                .queryParam("offset", 0)
+                .when().get("/api/v1/products").
+                then().statusCode(200)
+                .contentType(ContentType.JSON.withCharset("utf-8"))
+                .extract().jsonPath();
+
+        System.out.println("---- GET ME FIRST ID ------");
+        System.out.println("jp.getInt(\"[0].id\") = " + jp.getInt("[0].id"));
+        System.out.println("jp.getInt(\"id[0]\") = " + jp.getInt("id[0]"));
+
+        System.out.println("---- GET ME LAST ID ------");
+        System.out.println("jp.getInt(\"id[-1]\") = " + jp.getInt("id[-1]"));
+
+
+        //     * - Each product has id
+        //     * - Get all product ids
+        List<Integer> allIDs = jp.getList("id");
+        for (Integer eachID : allIDs) {
+            System.out.println("eachID = " + eachID);
+            Assertions.assertNotNull(eachID);
+        }
+
+        //     * - Each product has category id
+        List<Integer> allCategoryIDs = jp.getList("category.id");
+        for (Integer eachID : allCategoryIDs) {
+            System.out.println("CategoryID = " + eachID);
+            Assertions.assertNotNull(eachID);
+        }
+
+        //     * - Get all product title
+        List<String> allTitles = jp.getList("title");
+        for (String eachTitle : allTitles) {
+            System.out.println("eachTitle = " + eachTitle);
+        }
+
+        //     * - Get all category names
+        List<String> allCategoryNames = jp.getList("category.name");
+        for (String eachCatName : allCategoryNames) {
+            System.out.println("eachCatName = " + eachCatName);
+        }
+
+        // Get me all product title where product price more than 1000
+
 
     }
 }
