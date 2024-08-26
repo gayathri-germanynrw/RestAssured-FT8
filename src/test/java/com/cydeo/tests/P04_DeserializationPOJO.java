a03b79a39b4beb72f257954abaef043106c53a1b
mehmetCt <mehmet@cybertekschool.com>
2024-08-26 16:26:16 -0400

    Day03

---
+++ src/test/java/com/cydeo/tests/P04_DeserializationPOJO.java
@@ -0,0 +1,65 @@
+package com.cydeo.tests;
+
+import org.junit.jupiter.api.Test;
+
+public class P04_DeserializationPOJO {
+
+    /*
+        - ERGAST API ( Formula API )
+        - When user send request /status.json
+        - Then verify status code is 200
+        - And content type is application/json; charset=utf-8
+        - And total is 137
+        - And limit is 30
+        - And each status has statusId
+     */
+
+    /*
+
+    DESERIALIZATION
+
+      RESPONSE --> JSON (Javascript Object Notation)
+
+      let employee={
+                        "name":"Mike",
+                        "age":34,
+                        "gender":"Male"
+                    }
+
+      public class Employee{
+
+       private String name;
+       private int age;
+       private String gender;
+
+      }
+
+      JSON --> TO --> JAVA
+      XML  --> TO --> JAVA
+
+      How we will do that ?
+
+      1- Get All JSON --> JAVA Data Structure --> Map,List etc.
+
+      2- Get All JSON --> JAVA Custom Classes --> POJO (Plain Old Java Object)
+
+     Do we need to have any library for De-serialization/ Serialization ?
+
+      - Yes we need to have one databind || objectMapper
+         - Gson
+         - Jackson
+         - Yasson etc.
+
+         <dependency>
+            <groupId>com.fasterxml.jackson.core</groupId>
+            <artifactId>jackson-databind</artifactId>
+            <version>2.15.1</version>
+        </dependency>
+
+     */
+
+    @Test
+    public void task1() {
+
+    }
+}
